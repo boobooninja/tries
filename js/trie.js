a7581087e986ae0d09f8d1fa9af5bae17da82a14
Loren Hale <loren.hale@gmail.com>
2014-08-12 11:20:49 -0500

    insert and includes working

--- js/trie.js
+++ js/trie.js
@@ -1,6 +1,10 @@
 // Defines the node object
 var Node = function(options) {
-
+  options || (options = {});
+  this.value    = options.value;
+  this.stop     = options.stop;
+  this.parent   = options.parent;
+  this.children = options.children || {};
 };
 
 var Trie = function() {
@@ -8,23 +12,42 @@ var Trie = function() {
 
   // Inserts string into the Trie.
   this.insert = function(string) {
-
+    this._insert(string, this.head);
   };
 
   // Recursive function. Helper function for the insert function.
-  this._insert = function() {
-
+  this._insert = function(substring, node) {
+    var character = substring.slice(0,1); // Grab the first character
+    var newNode = node.children[character];
+    if (!newNode) {
+      newNode = new Node({ value: character, parent: node});
+      node.children[character] = newNode; // Set the child of the parent
+    }
+    if (substring.length == 1) { // If the string length is 1 set stop
+      newNode.stop = true;
+    } else { // If the string has more characters
+      this._insert(substring.slice(1), newNode); // Recursive call for the newly created node.
+    }
   };
 
   // Returns true if string is in the trie. Returns false otherwise.
   this.includes = function(string) {
-
+    return this._includes(string, this.head);
   };
 
   // Recursive function. Returns true if string is in the trie. Returns false
   // otherwise.
-  this._includes = function() {
-
+  this._includes = function(substring, node) {
+    var character = substring.slice(0,1); // Grab the first character
+    var foundNode = node.children[character];
+    if(foundNode) {
+      var stop = (substring.length - 1) == 0 && foundNode.stop;
+      console.log(stop);
+      if (stop) { return true; }
+      return this._includes(substring.slice(1), foundNode)
+    } else {
+      return false;
+    }
   };
 
   // Search for all strings that have 'prefix' as the prefix.
